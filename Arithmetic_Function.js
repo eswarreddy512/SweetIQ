f04930a9b8752b03c999e85bf71e9b8ea3766323
eswarreddy512 <eswarreddy512@gmail.com>
2018-07-03 20:23:24 -0400

    added Arithmetic_Function.js

---
+++ Arithmetic_Function.js
@@ -0,0 +1,31 @@
+exports.arthimetic_calculations = function(opeType, numberOne, NumberTwo) {
+
+  if(!isNaN(numberOne)&&!isNaN(NumberTwo)){
+    switch (opeType) {
+      case "+":
+        return numberOne + NumberTwo;
+        break;
+      case "-":
+        return numberOne - NumberTwo;
+        break;
+      case "*":
+        return numberOne * NumberTwo;
+        break;
+      case "/":
+        if (NumberTwo != 0) {
+          return numberOne / NumberTwo;
+        } else {
+          return "Can not divide by 0";
+        }
+        break;
+      default:
+        return "Wrong Operation Type";
+        break;
+    }
+  }
+  else{
+    return "Must pass numbers as params";
+  }   
+
+  
+  };
